cbfc1ffbf827612c6858e8bd83e9686b57730583
vwilko <wilkinsonvictoria@me.com>
2021-02-06 18:44:59 +0000

    Date updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+
 
 
 function displayTemperature (response){
@@ -7,7 +38,8 @@ function displayTemperature (response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-    let feelsElement = document.querySelector("feels");
+    let feelsElement = document.querySelector("#feels");
+    let dateElement = document.querySelector("#date")
     
 
     cityElement.innerHTML = response.data.name;
@@ -15,7 +47,8 @@ function displayTemperature (response){
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
-    feelsElement.innerHTML = response.data.main.feels_like;
+    feelsElement.innerHTML = Math.round(response.data.main.feels_like);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "99c6c9b126b6c2748213ca0867d33cb6";
