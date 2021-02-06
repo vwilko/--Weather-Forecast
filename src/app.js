f08693d54a43ecac44508661404aebe8af3ff3d1
vwilko <wilkinsonvictoria@me.com>
2021-02-06 12:14:15 +0000

    Api Added

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+
+function displayTemperature (response){
+    console.log (response.data)
+}
+
+let apiKey = "99c6c9b126b6c2748213ca0867d33cb6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.4418&lon=-94.0377&exclude=hourly,minutely&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
