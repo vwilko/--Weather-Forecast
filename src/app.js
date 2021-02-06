37c740b79fa1e799c25466cad4b20f7fec4ec41a
vwilko <wilkinsonvictoria@me.com>
2021-02-06 18:16:27 +0000

    Update real data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 
 
 function displayTemperature (response){
-    console.log (response.data)
+    console.log(response.data);
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let feelsElement = document.querySelector("feels");
+    
+
+    cityElement.innerHTML = response.data.name;
+    temperatureElement.innerHTML = Math.round(response.data.main.temp)
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+    feelsElement.innerHTML = response.data.main.feels_like;
 }
 
 let apiKey = "99c6c9b126b6c2748213ca0867d33cb6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.4418&lon=-94.0377&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
