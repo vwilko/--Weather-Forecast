ddb521db7ac73a5524ff6156c873301282033d12
vwilko <wilkinsonvictoria@me.com>
2021-02-06 13:11:32 +0000

    initial html

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ function displayTemperature (response){
 }
 
 let apiKey = "99c6c9b126b6c2748213ca0867d33cb6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.4418&lon=-94.0377&exclude=hourly,minutely&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.4418&lon=-94.0377&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
